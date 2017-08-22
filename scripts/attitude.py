939c0288d2beb5ff04cd44e80a1df3c08e6e4700
soonki <sungi628@outlook.jp>
2017-08-22 09:02:49 +0000

    test

--- scripts/attitude.py
+++ scripts/attitude.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import spidev
 import time
-import math
+import math as m
 import numpy as np
 from MPU9250 import MPU9250
 
@@ -8,22 +11,24 @@ def init():
     imu = MPU9250()
     imu.initialize()
     i=1
-    while i<=100
-        m6a,m6g=getMotion6()
-        g0+=m6g
+    g0=np.zeros(3)
+    while i<=100:
+        m6a,m6g=imu.getMotion6()
+        g0=g0+m6g
         i+=1
     return g0/100
 
 def getAcangle():
-        a,g=getMotion6()
+        a,g=imu.getMotion6()
+	a=a*10
         roll=m.atan(a[1]/a[2])
         pitch=m.atan(-a[0]/m.sqrt(m.pow(a[1],2)+m.pow(a[2],2)))
         ang=np.array([m.atan(-a[0]/m.sqrt(m.pow(a[1],2)+m.pow(a[2],2))),m.atan(a[1]/a[2]),0])
         return ang
 
 def getAcintGy(ang0,dt,gv):
-        a,g=getMotion6()
-        g=g-gv
+        a,g=imu.getMotion6()
+        g=(g-gv)/180*np.pi
         ang_a=np.array([m.atan(-a[0]/m.sqrt(a[1]*a[1]+a[2]*a[2])),m.atan(a[1]/a[2]),0])
         B=np.array([[0,m.cos(ang0[1]),-m.sin(ang0[1])],
 [1,m.sin(ang0[1])*m.tan(ang0[0]),m.cos(ang0[1])*m.tan(ang0[0])],
@@ -34,11 +39,11 @@ def getAcintGy(ang0,dt,gv):
 
 if __name__ == '__main__':
 	imu = MPU9250()
-    imu.initialize()
-    gv=init()
+	imu.initialize()
+	gv=init()
 	ang_0=np.zeros([3,])
         tau=0.2
-        ts=0.04
+        ts=0.01
         i=1
         while i<=1000:
                 ang_0+=getAcangle()
@@ -47,16 +52,13 @@ if __name__ == '__main__':
         ang_g0=ang_a0
 
         while(1):
-                #print getIMU()
                 t=time.time()
                 ang_a,ang_g,ang_v=getAcintGy(ang_g0,ts,gv)
                 ang_a=(ang_a0+ang_a*ts/tau)/(1+ts/tau) #ローパスフィルタ
                 ang_g[:2]=ang_g[:2]-(ang_g0[:2]+ang_g[:2]*ts/tau)/(1+ts/tau)
-#               print ang_a*180/np.pi,ang_g*180/np.pi
                 ang=ang_a+ang_g
                 print  "{:+7.3f}".format(ang[0]*180/np.pi),",","{:+7.3f}".format(ang[1]*180/np.pi),",","{:+7.3f}".format(ang[2]*180/np.pi)
-                #dt=time.time()-t
-#               print "{:+7.5f}".format(dt)
                 ang_g0=ang_g
                 ang_a0=ang_a
                 time.sleep(ts-time.time()+t)
+		#print time.time()-t
